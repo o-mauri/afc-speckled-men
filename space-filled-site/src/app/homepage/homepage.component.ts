576fef4574bfa8e17b5f6d37d612115075e4ad7b
o-mauri <o.mauri@outlook.com>
2023-02-26 21:40:34 +0000

    WIP

--- space-filled-site/src/app/homepage/homepage.component.ts
+++ space-filled-site/src/app/homepage/homepage.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import * as data from '../../assets/siteData.json'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit 
+{
+  homeScore: number = 6;
+  oppScore: number = 1;
+  oppName: string = "Saucy 6";
 
+
+
+
+  ngOnInit(): void {
+    var rawData = data;
+  }
 }
